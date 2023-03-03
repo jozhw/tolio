b4e1003036e669a732a4e26bf38c08b8bd3d4322
John Z. Wu <johnz.wu@Johns-MacBook-Air.local>
2023-03-02 22:38:09 -0600

    update: syling

--- test/test_gui_bridge.py
+++ test/test_gui_bridge.py
@@ -7,75 +7,77 @@ sys.path.insert(0, path)
 
 import unittest
 
+entry_dic_pass = {
+    "timestamp": "2014-07-03",
+    "amount": 10,
+    "price_USD": 10.00,
+    "name": "S&P500",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+entry_dic_fail_timestamp = {
+    "timestamp": "2014-0C-03",
+    "amount": 10,
+    "price_USD": 10.00,
+    "name": "S&P500",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+entry_dic_fail_amount = {
+    "timestamp": "2014-07-03",
+    "amount": "C",
+    "price_USD": 10.00,
+    "name": "S&P500",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+entry_dic_fail_price = {
+    "timestamp": "2014-07-03",
+    "amount": 10,
+    "price_USD": "C",
+    "name": "S&P500",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+
+entry_dic_fail_name = {
+    "timestamp": "2014-07-03",
+    "amount": 10,
+    "price_USD": 10,
+    "name": "",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+entry_dic_fail_to_institution = {
+    "timestamp": "2014-07-03",
+    "amount": 10,
+    "price_USD": 10,
+    "name": "S&P500",
+    "ticker": "SPY",
+    "institution_name": "Computershare",
+    "from_institution": "Computershare",
+    "to_institution_name": "Computershare",
+    "transaction_type": "Transfer"
+}
+
+
 class TestGuiFunctionalities(unittest.TestCase):
+    '''class to test gui functionalities'''
 
     # ================================= tests: insert _ into database functionalities =================================
 
     # check for the syntax of transaction entry
     def test_check_correct_values(self):
-        entry_dic_pass = {
-            "timestamp": "2014-07-03",
-            "amount": 10,
-            "price_USD": 10.00,
-            "name": "S&P500",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-        entry_dic_fail_timestamp = {
-            "timestamp": "2014-0C-03",
-            "amount": 10,
-            "price_USD": 10.00,
-            "name": "S&P500",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-        entry_dic_fail_amount = {
-            "timestamp": "2014-07-03",
-            "amount": "C",
-            "price_USD": 10.00,
-            "name": "S&P500",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-        entry_dic_fail_price = {
-            "timestamp": "2014-07-03",
-            "amount": 10,
-            "price_USD": "C",
-            "name": "S&P500",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-
-        entry_dic_fail_name = {
-            "timestamp": "2014-07-03",
-            "amount": 10,
-            "price_USD": 10,
-            "name": "",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-        entry_dic_fail_to_institution = {
-            "timestamp": "2014-07-03",
-            "amount": 10,
-            "price_USD": 10,
-            "name": "S&P500",
-            "ticker": "SPY",
-            "institution_name": "Computershare",
-            "from_institution": "Computershare",
-            "to_institution_name": "Computershare",
-            "transaction_type": "Transfer"
-        }
-
 
         self.assertTrue(check_correct_values(entry_dic_pass))
 
--- test/test_standardized_entry.py
+++ test/test_standardized_entry.py
@@ -9,86 +9,88 @@ import unittest
 
 from src.utils import StandardizeEntry
 
+
+###################### corrected values ########################
+correct_acquire_entry_dic = {
+    "timestamp": "2014-07-03",
+    "amount": 10.0,
+    "price_USD": 10.00,
+    "name": "Tesla Motors & S & P500",
+    "ticker": "TSLA",
+    "institution_name": "Computershare",
+    "transaction_type": "A"
+}
+
+correct_dispose_entry_dic = {
+        "timestamp": "2014-07-03",
+        "amount": -10.0,
+        "price_USD": -10.00,
+        "name": "Tesla",
+        "ticker": "TSLA",
+        "institution_name": "Computershare",
+        "transaction_type": "D"
+    }
+
+correct_regex_entry_dic = {
+    "timestamp": "2014-07-03",
+    "amount": 10.0,
+    "price_USD": 10.00,
+    "name": "Tesla Motors & S & P500",
+    "ticker": "TSLA",
+    "institution_name": "Computershare",
+    "transaction_type": "A"
+
+
+}
+
+###################### incorrected values ########################
+acquire_entry_dic = {
+    "timestamp": "2014-07-03",
+    "amount": -10.00,
+    "price_USD": -10.00,
+    "name": "Tesla Motors & S & P500",
+    "ticker": "TSLA",
+    "institution_name": "Computershare",
+    "transaction_type": "Acquire"
+}
+
+dispose_entry_dic = {
+        "timestamp": "2014-07-03",
+        "amount": 10.00,
+        "price_USD": 10.00,
+        "name": "Tesla",
+        "ticker": "TSLA",
+        "institution_name": "Computershare",
+        "transaction_type": "Dispose"
+}
+
+regex_entry_dic = {
+    "timestamp": "2014-07-03",
+    "amount": 10.00,
+    "price_USD": 10.00,
+    "name": "TEsla Motors & S & p500",
+    "ticker": "TsLA",
+    "institution_name": "computerShare",
+    "transaction_type": "Acquire"
+
+}
+
 class TestStandardizedEntries(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestStandardizedEntries, self).__init__(*args, **kwargs)
-        ###################### corrected values ########################
-        self.correct_acquire_entry_dic = {
-            "timestamp": "2014-07-03",
-            "amount": 10.0,
-            "price_USD": 10.00,
-            "name": "Tesla Motors & S & P500",
-            "ticker": "TSLA",
-            "institution_name": "Computershare",
-            "transaction_type": "A"
-        }
-
-        self.correct_dispose_entry_dic = {
-                "timestamp": "2014-07-03",
-                "amount": -10.0,
-                "price_USD": -10.00,
-                "name": "Tesla",
-                "ticker": "TSLA",
-                "institution_name": "Computershare",
-                "transaction_type": "D"
-            }
-
-        self.correct_regex_entry_dic = {
-            "timestamp": "2014-07-03",
-            "amount": 10.0,
-            "price_USD": 10.00,
-            "name": "Tesla Motors & S & P500",
-            "ticker": "TSLA",
-            "institution_name": "Computershare",
-            "transaction_type": "A"
-
-
-        }
-
-        ###################### incorrected values ########################
-        self.acquire_entry_dic = {
-            "timestamp": "2014-07-03",
-            "amount": -10.00,
-            "price_USD": -10.00,
-            "name": "Tesla Motors & S & P500",
-            "ticker": "TSLA",
-            "institution_name": "Computershare",
-            "transaction_type": "Acquire"
-        }
-
-        self.dispose_entry_dic = {
-                "timestamp": "2014-07-03",
-                "amount": 10.00,
-                "price_USD": 10.00,
-                "name": "Tesla",
-                "ticker": "TSLA",
-                "institution_name": "Computershare",
-                "transaction_type": "Dispose"
-        }
-
-        self.regex_entry_dic = {
-            "timestamp": "2014-07-03",
-            "amount": 10.00,
-            "price_USD": 10.00,
-            "name": "TEsla Motors & S & p500",
-            "ticker": "TsLA",
-            "institution_name": "computerShare",
-            "transaction_type": "Acquire"
-
-        }
 
         ###################### create class objects to be tested ########################
-        self.acquire_standardize = StandardizeEntry(self.acquire_entry_dic)
-        self.dispose_standardize = StandardizeEntry(self.dispose_entry_dic)
-        self.regex_standardize = StandardizeEntry(self.regex_entry_dic)
+        self.acquire_standardize = StandardizeEntry(acquire_entry_dic)
+        self.dispose_standardize = StandardizeEntry(dispose_entry_dic)
+        self.regex_standardize = StandardizeEntry(regex_entry_dic)
 
     def test_regex_sub(self):
-        self.assertEqual(self.regex_standardize.regex_sub(), self.correct_regex_entry_dic)
+        self.assertEqual(self.regex_standardize.regex_sub(), correct_regex_entry_dic)
 
 
     def test_change_value_sign(self):
-        self.assertEqual(self.acquire_standardize.change_value_sign(), self.correct_acquire_entry_dic)
-        self.assertEqual(self.dispose_standardize.change_value_sign(), self.correct_dispose_entry_dic)
+        self.assertEqual(self.acquire_standardize.change_value_sign(), correct_acquire_entry_dic)
+        self.assertEqual(self.dispose_standardize.change_value_sign(), correct_dispose_entry_dic)
 
 if __name__ == "__main__":
     unittest.main()
